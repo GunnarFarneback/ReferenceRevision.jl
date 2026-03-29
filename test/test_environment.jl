a20bdf78a8eadfa9067e7095c0dde3e6a7442909
Gunnar Farnebäck <gunnar@lysator.liu.se>
2026-03-29 22:10:05 +0200

    Add more tests and fix a bug.

---
+++ test/test_environment.jl
@@ -0,0 +1,51 @@
+using UUIDs: uuid4
+
+# In the directory `dir`, generate a small package with an additional
+# environment in a subdirectory. Make a couple of commits to play
+# with.
+function create_test_environment(dir)
+    write_file(dir, "Project.toml",
+               """
+               name = "TestPackage"
+               uuid = "$(uuid4())"
+               version = "0.0.1"
+               """)
+    src = joinpath(dir, "src")
+    mkpath(src)
+    write_file(src, "TestPackage.jl",
+               """
+               module TestPackage
+               export f
+               f(x) = x .+ 1
+               end
+               """)
+    run(`git -C $dir init`)
+    run(`git -C $dir config user.email "ci@ci"`)
+    run(`git -C $dir config user.name ci`)
+    run(`git -C $dir add .`)
+    run(`git -C $dir commit -m "."`)
+    run(`git -C $dir tag commit1`)
+
+    write_file(src, "TestPackage.jl",
+               """
+               module TestPackage
+               export f
+               f(x) = x .+ 2
+               end
+               """)
+    run(`git -C $dir commit -a -m "."`)
+    run(`git -C $dir tag commit2`)
+
+    benchmark = joinpath(dir, "benchmark")
+    mkpath(benchmark)
+    write_file(benchmark, "Project.toml",
+               """
+               [deps]
+               BenchmarkTools = "6e4b80f9-dd63-53aa-95a3-0cdb28fa8baf"
+               """)
+    run(`git -C $dir add benchmark`)
+    run(`git -C $dir commit -m "."`)
+    run(`git -C $dir tag commit3`)
+end
+
+write_file(dir, filename, contents) = write(joinpath(dir, filename), contents)
